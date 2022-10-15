2025e40844b80f81ba118298acf54a033da353d6
algonacci <algonacci1@gmail.com>
2022-10-15 12:51:45 +0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>Hello, Welcome to my Flask App!</h1>"
+
+
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+        host="0.0.0.0"
+        # port=
+        # ssl_context=()
+    )
