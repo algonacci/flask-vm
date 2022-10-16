5fdef28c82de7b06e856c32dc5367fffca04430d
algonacci <algonacci1@gmail.com>
2022-10-16 11:24:25 +0700

    add

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+upload_folder = './static/upload'
 
 
-@app.route("/")
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return "<h1>Hello, Welcome to my Flask App!</h1>"
+    if request.method == "POST":
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(upload_folder, filename))
+            saved_image = os.path.join(upload_folder, filename)
+            return render_template("index.html", image=saved_image)
+    else:
+        return render_template("index.html")
+
+
+@app.route("/result", methods=["GET", "POST"])
+def result():
+    if request.method == "POST":
+        result = request.form["name"]
+        return render_template("result.html", result=result)
+    else:
+        return render_template("result.html")
 
 
 if __name__ == "__main__":
